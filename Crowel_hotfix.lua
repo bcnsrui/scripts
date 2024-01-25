38be63c70ece197cc35e7bea34ca7aa0381b1830
Crowel <faithtaeho@naver.com>
2024-01-25 14:34:45 +0900

    업데이트: 크로우엘님 유틸리티
    - 저번 핫픽스에서 추가로 발견된 버그를 수정

--- Crowel_hotfix.lua
+++ Crowel_hotfix.lua
@@ -3,8 +3,8 @@ if Crowel_Config and not Crowel_Config[CC_USE_HOTFIX] then
 	USE_HOTFIX = false
 end
 if Crowel_Config and Crowel_Config[CC_USE_HOTFIX] and type(Crowel_Config[CC_USE_HOTFIX])=="function" then
-	--두 사람의 합의, 혹은 코인 토스 등의 요소로 결정할 경우
-	--차후 작성 예정
+	--두 사람의 합의, 혹은 코인 토스 등의 요소로 결정할 경우 (차후 작성 예정)
+	--Unsupported yet
 	USE_HOTFIX = false
 end
 
@@ -25,14 +25,18 @@ local nexttg_cleanup=function(e,v)
 		current_te_or_tc=te_or_tc
 	end
 end
-local EnableCurrentEffectCheck=function()
-	for str,func in pairs(Effect) do
-		if string.sub(str,1,3)=="Set" then
+local EnableCurrentEffectCheck=function(...)
+	--for str,func in pairs(Effect) do
+	--	if string.sub(str,1,3)=="Set" then
+	for idx,str in ipairs({...}) do
+		local func=Effect[str]
+		if func and string.sub(str,1,3)=="Set" then
+			--
 			local eset=func
 			Effect[str]=function(e,...)
 				local args={...}
 				local args2={}
-				for pos,f_or_v in ipairs(args) do
+				for pos,f_or_v in pairs(args) do
 					if type(f_or_v)=="function" then
 						local f2=function(e2,v2,...)
 							if e2 and type(e2)=="Effect" then
@@ -50,13 +54,13 @@ local EnableCurrentEffectCheck=function()
 						args2[pos]=f_or_v
 					end
 				end
-				local returns={eset(e,table.unpack(args2))}
-				return table.unpack(returns)
+				return eset(e,table.unpack(args2))
 			end
+			--
 		end
 	end
 end
-EnableCurrentEffectCheck()
+EnableCurrentEffectCheck("SetCost","SetTarget","SetOperation")
 
 
 --Bug hotfix : Stacking LP Cost
@@ -72,7 +76,7 @@ table.insert(nexttg_cleanup_table,function(e,_)
 end)
 Duel.CheckLPCost=function(player,cost)
 	--use usual return value if USE_HOTFIX is false, or the cost itself is unable to pay
-	if not USE_HOTFIX then return table.unpack({clpc(player,cost)}) end
+	if not USE_HOTFIX or not current_effect then return clpc(player,cost) end
 	if not clpc(player,cost) then return false end
 	--calculate total LP cost
 	local id=(Effect.GetOwner(current_effect)):GetOriginalCode()
