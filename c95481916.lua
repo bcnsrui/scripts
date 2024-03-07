d3df6c3f8c1213b48bfaa5e94cf9c910e1289aeb
makabeP <87193249+makabeP@users.noreply.github.com>
2024-03-07 19:39:18 +0900

    Add files via upload

--- c95481916.lua
+++ c95481916.lua
@@ -1,14 +1,15 @@
 --인챈트릭스 시긴
-function c95481916.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(46659709,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCost(c95481916.spcost)
-	e1:SetTarget(c95481916.sptg)
-	e1:SetOperation(c95481916.spop)
+	e1:SetCost(s.spcost)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 	--copy
 	local e2=Effect.CreateEffect(c)
@@ -16,50 +17,50 @@ function c95481916.initial_effect(c)
 	e2:SetCategory(CATEGORY_REMOVE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCountLimit(1,95481916)
+	e2:SetCountLimit(1,id)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTarget(c95481916.target)
-	e2:SetOperation(c95481916.op1)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.op1)
 	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_GRAVE)
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DRAW)
-	e3:SetCountLimit(1,95481916)
-	e3:SetCost(c95481916.cost2)
-	e3:SetTarget(c95481916.target)
-	e3:SetOperation(c95481916.op2)
+	e3:SetCountLimit(1,id)
+	e3:SetCost(s.cost2)
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.op2)
 	c:RegisterEffect(e3)
 end
-function c95481916.cfilter(c)
+function s.cfilter(c)
 	return c:IsSetCard(0xd49) and c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
 end
-function c95481916.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c95481916.cfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
-	Duel.DiscardHand(tp,c95481916.cfilter,1,1,REASON_COST,e:GetHandler())
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,s.cfilter,1,1,REASON_COST,e:GetHandler())
 end
-function c95481916.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function c95481916.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 end
-function c95481916.spfilter(c,e,tp)
+function s.spfilter(c,e,tp)
 	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
 end
-function c95481916.filter2(c,e,tp)
-	return c:IsFacedown() or c95481916.spfilter(c,e,tp)
+function s.filter2(c,e,tp)
+	return c:IsFacedown() or s.spfilter(c,e,tp)
 end
-function c95481916.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=Duel.GetFieldGroup(tp,0,LOCATION_EXTRA)
-	if chk==0 then return g:GetCount()>0 and g:IsExists(c95481916.filter2,1,nil,e,tp) end
+	if chk==0 then return g:GetCount()>0 and g:IsExists(s.filter2,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,1-tp,LOCATION_EXTRA)
 end
-function c95481916.op1(e,tp,eg,ep,ev,re,r,rp)
+function s.op1(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetFieldGroup(tp,0,LOCATION_EXTRA)
 	local tc=g:RandomSelect(tp,1):GetFirst()
@@ -85,7 +86,7 @@ function c95481916.op1(e,tp,eg,ep,ev,re,r,rp)
 	end
 	Duel.SpecialSummonComplete()
 end
-function c95481916.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then
 		return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) and
@@ -96,12 +97,12 @@ function c95481916.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)
 	Duel.Remove(c,POS_FACEUP,REASON_COST)
 end
-function c95481916.op2(e,tp,eg,ep,ev,re,r,rp)
+function s.op2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetFieldGroup(tp,0,LOCATION_EXTRA)
 	Duel.ConfirmCards(tp,g)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sg=g:FilterSelect(tp,c95481916.spfilter,1,1,nil,tp)
+	local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,0,LOCATION_EXTRA,1,1,nil,e,tp)
 	local tc=sg:GetFirst()
 	if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
 		local e1=Effect.CreateEffect(c)
